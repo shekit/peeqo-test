42b99df40978e83a577272b8a81b024771a6e157
shekit <as7790@nyu.edu>
2016-12-20 02:01:48 -0500

    new folder without speech closing error, cleared up duplicate modules in package.json - was possibly causing the google speech bug

---
+++ app/js/global.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+	// Add directories to module search path, Prevents requiring local imports like ./js/lib/abc.js - instead becomes js/lib/abc.js
+	require('app-module-path').addPath(__dirname);
+	const path = require('path')
+	global.isSleeping = false
+	const ipcRenderer = require('electron').ipcRenderer
+	
+	const onlineStatus = require('js/check-online-status')()
+	
+	////**** WIFI SCANNING ****////
+
+	//const wifi = require('js/wifi-scan.js')();
+
+	//const mic = require('js/listen')()
+	//const hotword = require('js/hotword')()
+	
+	const socket = require('js/socket')()
+
+	const event = require('js/events')
+	const listeners = require('js/listeners')()
+
+	event.emit("show-div","eyeWrapper")
+	event.emit("start-blinking")
+
+	//mic.start(hotword.detector)
+
+	$("body").on("click", function(e){
+		
+		var boop = path.join(__dirname, './images', 'local', 'r_boop', 'boop.gif')
+		event.emit("servo","boop")
+		event.emit("stop-music")
+		event.emit("play-gif",boop)
+
+		setTimeout(function(){
+			event.emit("gif-timer-ended",null)
+		},2000)
+	})
+
+	ipcRenderer.on("hotword", function(event,arg){
+		console.log("HOTWORD",arg)
+	})
+})
---
+++ app/js/productivity.js
@@ -0,0 +1,60 @@
+const event = require('js/events')
+
+module.exports = function(){
+
+	var productivity = {}
+
+	productivity.list = {}
+
+	productivity.blockSite = function(site){
+		var lower = site.toLowerCase()
+		productivity.list[lower] = true
+	}
+
+	productivity.unblockSite = function(site){
+		var lower = site.toLowerCase()
+		productivity.list[lower] = false
+	}
+
+	productivity.checkBlocked = function(site){
+		var lower = site.toLowerCase()
+		if(lower in productivity.list && productivity.list[lower] == true){
+			event.emit("is-blocked", true)
+		}
+		event.emit("is-blocked",false)
+	}
+
+	productivity.isBlocked = function(){
+		var obj = {
+			gif_type:"local",  //local/remote
+			gif_category:"r_angry",
+			gif_url: null,
+			gif_loop_forever: false,
+			servo:"angry",
+			led:"fadeRed",
+			sound:"angry",
+			sound_loop_forever: false,
+			callback: function(){
+				var anim = {
+					gif_type:"local",  //local/remote
+					gif_category:"r_warning",
+					gif_url: null,
+					gif_loop_forever: false,
+					servo:"lookup",
+					led: "error",
+					sound:null,
+					sound_loop_forever: false,
+					callback: null
+				}
+
+				setTimeout(function(){
+					event.emit("animate", anim)
+				},1000)
+			}
+		}
+
+		event.emit("animate", obj)
+	}
+
+	return productivity
+}
